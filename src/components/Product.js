0e977562a2c693bdbfcb825193cb41db6633f0e4
LynAudubon <lyn_audubon07@yahoo.com>
2021-09-17 16:34:01 -0700

    no redux

--- src/components/Product.js
+++ src/components/Product.js
@@ -16,8 +16,8 @@ const Product = () => {
             Asplenium nidus forms large simple fronds visually similar to banana leaves, with the fronds growing to 50–150 centimetres (20–59 in) long and 10–20 centimetres (3.9–7.9 in) broad. A. nidus belongs to the \"Neottopteris clade\" members of which generally have somewhat leathery leaf tissue. The fronds roll back as they brown and create a massive leaf nest in the branches and trunks of trees. Asplenium nidus is native to east tropical Africa (in Tanzania, inclusive of the Zanzibar Archipelago); temperate and tropical Asia (in Indonesia; East Timor; the prefecture of Kyushu, and the Ryukyu Islands of Japan; Malaysia; the Philippines; Taiwan; and Thailand); and in Australasia (in the northern part of Queensland in Australia).[8]  It thrives in warm, humid areas in partial to full shade. It dislikes direct sunlight and likes to be in full shade on the north facing garden wall.With a minimum temperature of 10 °C (50 °F), Asplenium nidus is widely cultivated in temperate regions as a houseplant.
             </p>
         </div>
-        <div>
-            <p>$30.99</p>
+        <div className='info_price'>
+            <p> Price: $30.99</p>
             <Link className="info_button">Details</Link>
         </div>
         </div>
