5bea247e3c3d233f4f938c479a8a77ffe1b9cd2c
LynAudubon <lyn_audubon07@yahoo.com>
2021-09-17 14:46:56 -0700

    Fri-4

--- src/index.js
+++ src/index.js
@@ -7,11 +7,13 @@ import './index.css';
 import {} from 'react-dom'
 import store from './redux/store'
 
+import {Provider} from 'react-redux'
+
 const appElement = document.getElementById('root');
 
 ReactDOM.render (
-<Provider store={store}>
-    <App /> 
-</Provider>,
+  <Provider store={store}>
+       <App />
+  </Provider>,
 appElement
 );
